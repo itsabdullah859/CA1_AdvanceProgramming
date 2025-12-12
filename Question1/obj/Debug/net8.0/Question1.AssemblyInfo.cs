95dac984bc115f36d723232d8394ff048ff6533b
Abdullah Babar <itsabdullah859@gmail.com>
2025-12-12 17:37:14 +0000

    updating folder

--- Question1/obj/Debug/net8.0/Question1.AssemblyInfo.cs
+++ Question1/obj/Debug/net8.0/Question1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Question1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32e92b0cea0ea05ea443feabbecadf05b1eeb2aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27aca4cf3fa38f6789a73bf6f284f414cc7359db")]
 [assembly: System.Reflection.AssemblyProductAttribute("Question1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Question1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
